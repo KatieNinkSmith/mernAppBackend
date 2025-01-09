15b4e92712619f0a23f77b3e165d8a6d5cea7c7b
Katie Nink Smith <katiej.smith@outlook.com>
2025-01-09 09:10:56 -0700

    login is working

--- controllers/user.mjs
+++ controllers/user.mjs
@@ -1,5 +1,6 @@
 import User from "../models/user.mjs";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 async function create(req, res) {
   try {
@@ -8,6 +9,24 @@ async function create(req, res) {
     // the token will be a string
     const token = createJWT(createdUser);
     res.status(200).json(token);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+async function login(req, res) {
+  try {
+    // query the database to find a user with the email provided
+    const user = await User.findOne({ email: req.body.email });
+    // if the email does not exsist, throw an error
+    if (!user) throw new Error("User not found");
+    // if we find the user, compare the password, but it is stored encrypted
+    // 1st argument is from the credentials that the user typed in
+    // 2nd arguemtn is what is stored in the database
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error("Invalid password");
+    const token = createJWT(user);
+    res.json(token);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -21,4 +40,4 @@ function createJWT(user) {
     { expiresIn: "24h" }
   );
 }
-export default { create, createJWT };
+export default { create, createJWT, login };
--- routes/user.mjs
+++ routes/user.mjs
@@ -7,4 +7,7 @@ import userController from "../controllers/user.mjs";
 // this corresponds to regisiter on the frontend because register means add a new user
 router.post("/", userController.create);
 
+// this corresponds to login on the frontend
+router.post("/login", userController.login);
+
 export default router;
